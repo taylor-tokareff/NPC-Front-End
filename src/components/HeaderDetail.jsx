da993bc8bce2feff6fce82bf19d2898f610626a7
taylor-tokareff <taylortokareff@gmail.com>
2021-08-08 21:05:03 -0700

    npc form complete

---
+++ src/components/HeaderDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HeaderDetail = () => (
+  <div>
+    <Link to="/">Home</Link>
+    <h1>NPC Library</h1>
+  </div>
+);
+
+
+
+
+export default HeaderDetail;
