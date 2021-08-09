749be0773f38929f0bd291765ce62e0cc363f60f
taylor-tokareff <taylortokareff@gmail.com>
2021-08-09 10:04:53 -0700

    breakfast commit

--- src/components/HeaderDetail.jsx
+++ src/components/HeaderDetail.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const HeaderDetail = () => (
   <div>
     <Link to="/">Home</Link>
+    <Link to="/create">Make an Npc</Link>
     <h1>NPC Library</h1>
   </div>
 );
